3aa67047a1cda61ca6ee3472df1e59c5bfd673ff
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-04-12 13:41:11 -0700

    adding dispatch table for print functions

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,7 +6,7 @@
 /*   By: ale-goff <ale-goff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/31 19:59:06 by ale-goff          #+#    #+#             */
-/*   Updated: 2019/04/07 10:21:05 by ale-goff         ###   ########.fr       */
+/*   Updated: 2019/04/12 10:53:05 by ale-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,10 @@ typedef struct		s_hash
 	uint32_t		d;
 	uint32_t		f;
 	uint32_t		g;
-	uint32_t		h0;
-	uint32_t		h1;
-	uint32_t		h2;
-	uint32_t		h3;
-
 }					t_hash;
 
 typedef struct		s_hash256
 {
-	unsigned int		h0;
-	unsigned int		h1;
-	unsigned int		h2;
-	unsigned int		h3;
-	unsigned int		h4;
-	unsigned int		h5;
-	unsigned int		h6;
-	unsigned int		h7;
 	unsigned int		a;
 	unsigned int		b;
 	unsigned int		c;
@@ -53,6 +40,19 @@ typedef struct		s_hash256
 	unsigned int		g;
 	unsigned int		h;
 }					t_hash256;
+
+typedef struct		s_hash512
+{
+	uint64_t		a;
+	uint64_t		b;
+	uint64_t		c;
+	uint64_t		d;
+	uint64_t		e;
+	uint64_t		f;
+	uint64_t		g;
+	uint64_t		h;
+
+}					t_hash512;
 
 typedef struct		s_flags
 {
@@ -93,7 +93,14 @@ typedef struct		s_sha256
 	size_t			len_message;
 }					t_sha256;
 
+typedef struct		s_sha512
+{
+	unsigned char	*message;
+	size_t			len_message;
+}					t_sha512;
+
 typedef void		(*t_hashing)(t_ssl *ssl);
+typedef void		(*t_print)(int status);
 
 /*
 ** ERROR
@@ -116,6 +123,8 @@ void				append_file(t_lst **lst, char *str, t_flags *flags);
 t_md5				*init_md5(int len);
 void				init_hash256(t_hash256 *hash256);
 t_sha256			*init_sha(int len);
+void				init_hash512(t_hash512 *hash);
+t_sha512			*init_sha512(int newlen);
 
 /*
 ** PARSING
@@ -130,6 +139,7 @@ int					parsing_stdin(t_lst **list, t_flags *flags);
 
 t_flags				*init_flags();
 void				parse_flags(t_flags *flags, t_lst **lst, char **av);
+int					is_flags_activated(t_flags *flag, t_lst *lst);
 
 /*
 ** PRINTING FUNC
@@ -144,14 +154,15 @@ void				print_flags(t_flags *flags);
 
 void				md5_hash(t_ssl *ssl);
 void				sha256_hash(t_ssl *ssl);
+void        		sha512_hash(t_ssl *ssl);
 
 /*
 ** PRINTING FUNCTIONS
 */
 
-void				print_func(t_lst *lst, t_hash *hash, t_hash256 *hash256,
-					t_ssl *ssl);
-void				print_sha(t_hash256 *hash256, int status);
-void				swap_print_md5(t_hash *hash, int status);
+void				print_func(t_lst *lst, t_ssl *ssl);
+void				print_sha(int status);
+void				print_sha512(int status);
+void				swap_print_md5(int status);
 
 #endif
